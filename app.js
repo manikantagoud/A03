d6375b6fa1a755c220116646bcbfc5e2fac054ba
Manikanta goud Bandari <manikantagoud97@gmail.com>
2018-10-28 16:17:57 -0500

    all

--- app.js
+++ app.js
@@ -1,83 +1,101 @@
-const path = require("path")
-const express = require("express")
-const logger = require("morgan")
-const bodyParser = require("body-parser") // simplifies access to request body
-const fs = require('fs')  // NEW - this is required
-const app = express()  // make express app
-const http = require('http').Server(app)  // inject app into the server
-const port = 8081;
-// ADD THESE COMMENTS AND IMPLEMENTATION HERE 
-// 1 set up the view engine
-// 2 manage our entries
-// 3 set up the logger
-// 4 handle valid GET requests
-// 5 handle valid POST request (not required to fully work)
-// 6 respond with 404 if a bad URI is requested
+const path = require('path')
+const express = require('express')
+const logger = require('morgan')
+const bodyParser = require('body-parser') 
+const app = express()  
+const port = process.env.PORT || 8081
+const logfile = '/access.log'
 
-// 1 set up the view engine
-app.set("views", path.resolve(__dirname, "views")) // path to views
-app.set("view engine", "ejs") // specify our view
 
-// 2 include public assets and use bodyParser
-// Node uses __dirname for the The directory name of the current module.
-app.use(express.static(__dirname + '/public'));
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
+const fs = require('fs')
+const nodemailer = require('nodemailer')
+const mg = require('nodemailer-mailgun-transport')
 
-// 3 log requests to stdout and also
-// log HTTP requests to a file using the standard Apache combined format
-// see https://github.com/expressjs/morgan for more
-var accessLogStream = fs.createWriteStream(__dirname + '/access.log', { flags: 'a' });
-app.use(logger('dev'));
-app.use(logger('combined', { stream: accessLogStream }));
-// 4 http GET default page at /
-app.get("/", function (req, res) {
-    //res.sendFile(path.join(__dirname + '/assets/index.html'))
-    res.render("index.ejs")
+
+
+console.log(process.env);
+
+
+app.set('views', path.resolve(__dirname, 'views')) 
+app.set('view engine', 'ejs')
+
+
+app.use(express.static('public'))
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
+
+var accessLogStream = fs.createWriteStream(path.join(__dirname, logfile), { flags: 'a' })
+app.use(logger('dev'))
+app.use(logger('combined', { stream: accessLogStream }))
+
+
+
+app.get('/', function (req, res) {
+
+  res.render('index.ejs')
 })
 
-// 4 http GET /tic-tac-toe
-app.get("/cacluatingnoofdays", function (req, res) {
-    res.render("cacluatingnoofdays.ejs")
+app.get('/index', function (req, res) {
+  res.render('index.ejs')
+})
+app.get('/game', function (req, res) {
+  res.render('calcluatingnoofdays.ejs')
+})
+app.get('/contact', function (req, res) {
+  res.render('contact-me.ejs')
 })
 
-// 4 http GET /about
-app.get("/index", function (req, res) {
-    res.render("index.ejs")
+
+
+
+app.post('/contactus', function (req, res) {
+  const name = req.body.name
+  const email = req.body.email
+  const question = req.body.question
+  const isError = false
+
+  // logs to the terminal window (not the browser)
+  const s = '\nCONTACT FORM DATA: ' + name + ' ' + email + ' ' + question+  '\n'
+  console.log(s)
+
+  const mailOptions = {
+    from: 'Manikanta goud   <manikantagoud97@gmail.com>', // sender address
+    to: 's533898@nwmissouri.edu, manikantagoud97@gmail.com', // list of receivers
+    subject: 'Message from Website Contact page', // Subject line
+    text: s,
+    err: isError
+  }
+
+  try {
+    const auth = require('./config.json')
+    // create transporter object capable of sending email using the default SMTP transport
+    const transporter = nodemailer.createTransport(mg(auth))
+
+    transporter.sendMail(mailOptions, function (error, info) {
+      if (error) {
+        console.log('\nERROR: ' + error + '\n')
+        res.render('contact-error.ejs')
+      } else {
+        console.log('Sending email...')
+        if (info) { console.log('\nres SENT: ' + info.res + '\n') }
+        res.render('contact-confirm.ejs')
+      }
+    })
+  }
+  catch (e) {
+    console.log(e.message)
+    res.render('contact-error.ejs')
+  }
 })
 
-// 4 http GET /contact
-app.get("/contact-me", function (req, res) {
-    res.render("contact-me.ejs")
-})
-// 5 http POST /contact
-app.post("/contact", function (req, res) {
-    const name = req.body.inputname;
-    const email = req.body.inputemail;
-    const company = req.body.inputcompany;
-    const comment = req.body.inputcomment;
-    const isError = true;
-
-    // setup e-mail data with unicode symbols
-    const mailOptions = {
-        from: '"Manikanta Goud" <manikantagoud97@gmail.com>', // sender address
-        to: 'dcase@nwmissouri.edu, denisecase@gmail.com', // list of receivers
-        subject: 'Message from Website Contact page', // Subject line
-        text: comment,
-        err: isError
-    }
-
-    // logs to the terminal window (not the browser)
-    console.log('\nCONTACT FORM DATA: ' + name + ' ' + email + ' ' + comment + '\n');
-})
-// 6 this will execute for all unknown URIs not specifically handled
 app.get(function (req, res) {
-    res.render("404")
+  res.render('404.ejs')
 })
 
-// Listen for an application request on designated port
 
-// Listen for an application request on port 8081
+
 app.listen(port, function () {
-    console.log('Web app started and listening on http://localhost:' + port)
+  console.log('\nWeb app started and listening on http://localhost:' + port + '.')
+  console.log('\nLogs will be sent to this terminal and ' + logfile + '.')
+  console.log('\nKeep this open while serving, and use CTRL-C to quit.')
 })
